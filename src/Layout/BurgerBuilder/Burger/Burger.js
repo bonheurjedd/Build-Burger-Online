40ab3ab48cf775626701f20d564b44785064f2cc
jed <nsengiyumvabonheur@gmail.com>
2021-01-05 22:34:12 -0500

    2021.01.05

--- src/Layout/BurgerBuilder/Burger/Burger.js
+++ src/Layout/BurgerBuilder/Burger/Burger.js
@@ -1,12 +1,27 @@
 import React from 'react';
 import BurgerIngredients from './BurgerIngredients/BurgerIngredients';
 import classes from './Burger.module.css';
-const Burger = () => {
+const Burger = (props) => {
+ let transfromeingredient = Object.keys(props.burgerIngredients)
+  .map((ingredients, igKey) => {
+   return [...Array(props.burgerIngredients[ingredients])]
+    .map((_, i) => {
+     return (
+      <BurgerIngredients keys={i + 1} type={ingredients} />
+     )
+    })
+  }).reduce((arr, el) => {
+   return arr.concat(el)
+  })
+
+ if (transfromeingredient === 0) {
+  transfromeingredient = <p>Start Adding Ingredients !</p>
+ }
  return (
   <>
    <div className={classes.Burger}>
     <BurgerIngredients type="bread-top" />
-    <div><p>Ingredients</p></div>
+    {transfromeingredient}
     <BurgerIngredients type="bread-bottom" />
    </div>
   </>
--- src/Layout/BurgerBuilder/BurgerBuilder.js
+++ src/Layout/BurgerBuilder/BurgerBuilder.js
@@ -1,11 +1,20 @@
 import React, { Component } from 'react';
 import Burger from './Burger/Burger';
 class BurgerBuilder extends Component {
+ state = {
+  ingredients: {
+   salad: 1,
+   meat: 1,
+   bacon: 1,
+   cheese: 1,
+  }
+ }
+
  render() {
 
   return (
    <>
-    <Burger />
+    <Burger burgerIngredients={this.state.ingredients} />
     <div>BurgerControls</div>
    </>
   )
