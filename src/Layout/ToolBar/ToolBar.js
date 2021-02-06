5d2bb2663d6271de734c995a00c702dc91cf5647
jed <nsengiyumvabonheur@gmail.com>
2021-02-06 02:50:08 -0500

    More Updates

--- src/Layout/ToolBar/ToolBar.js
+++ src/Layout/ToolBar/ToolBar.js
@@ -1,36 +1,57 @@
-import React from 'react';
+import React, { Component } from 'react';
 import classes from './ToolBarStyle.module.css';
 import logo from '../Assets/images/burger-logo.png'
-const Toolbar = () => {
- return (
-  <>
-   <div className={classes.ToolbarWrapper}>
-    <div className={classes.OnlyMobile}>
-     <span></span>
-     <span></span>
-     <span></span>
+import BackOrder from '../BurgerBuilder/BackOrder/BackOrder';
+
+class ToolBar extends Component {
+ state = {
+  BackOrderToggle: false
+ }
+ showBackorder = () => {
+  this.setState({ BackOrderToggle: true });
+ }
+ hideBackorder = () => {
+  this.setState({ BackOrderToggle: false });
+  console.log("ajljdhfljahdk")
+ }
+ render() {
+  let shower = null;
+  if (this.state.BackOrderToggle) {
+   shower = <BackOrder />
+  }
+  return (
+   <>
+    <div onClick={this.hideBackorder}>
+     {shower}
     </div>
-    <div className={classes.LogoWrapper}>
-     <img src={logo} />
-    </div>
-    <div className={classes.NavWrapperDeskop}>
-     <div className={classes.LogoWrapperMobile}>
-      <img src={logo} />
+    <div className={classes.ToolbarWrapper}>
+     <div onClick={this.showBackorder} className={classes.OnlyMobile}>
+      <span></span>
+      <span></span>
+      <span></span>
      </div>
-     <nav>
-      <ul>
-       <li>
-        <a href="#">Build Burger</a>
-       </li>
-       <li>
-        <a href="#">Checkout</a>
-       </li>
-      </ul>
-     </nav>
+     <div className={classes.LogoWrapper}>
+      <img alt="logo" src={logo} />
+     </div>
+     <div className={this.state.BackOrderToggle ? classes.Drawer : classes.NavWrapperDeskop}>
+      <div className={this.state.BackOrderToggle ? classes.LogoWrapperMobile : classes.LogoWrapperMobileHide}>
+       <img alt="logo" src={logo} />
+      </div>
+      <nav>
+       <ul>
+        <li>
+         <a href="/">Build Burger</a>
+        </li>
+        <li>
+         <a href="/">Checkout</a>
+        </li>
+       </ul>
+      </nav>
+     </div>
     </div>
-   </div>
-  </>
- )
+   </>
+  )
+ }
 }
 
-export default Toolbar;
+export default ToolBar;
