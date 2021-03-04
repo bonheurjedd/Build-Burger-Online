9559b4c4108af6d8bd910d68c8d768db0431d881
jed <nsengiyumvabonheur@gmail.com>
2021-03-04 16:17:53 -0500

    Working on Routes

--- src/Layout/BurgerBuilder/BurgerControls/Buttons/Buttons.js
+++ src/Layout/BurgerBuilder/BurgerControls/Buttons/Buttons.js
@@ -3,7 +3,7 @@ import classes from './Buttons.module.css'
 
 const Buttons = (props) => {
  return (
-  <div className={classes.Buttons}>{props.children}</div>
+  <div className={[classes['Buttons'], classes[props.btnType]].join(' ')}>{props.children}</div>
  )
 }
 export default Buttons;
